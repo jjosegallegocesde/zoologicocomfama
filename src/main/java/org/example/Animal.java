7dcbc07505a5ec18a5aa9e1ac5406b088ec74665
205 <205@academicos.cesde.edu.co>
2023-02-21 20:50:42 -0500

    se crea proyecto zoologico

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Animal {
+
+    //ATRIBUTOS
+    Integer edad;
+    String nombre;
+    String especie;
+    Double peso;
+    Double estatura;
+    String colorPredominante;
+    Boolean genero;
+    Integer numeroPatas;
+    String dieta;
+    String clasificacion; //domestico o salvaje
+
+
+
+    //METODOS ESPECIALES
+
+    public Animal() {
+    }
+
+    public Animal(Integer edad, String nombre, String especie, Double peso, Double estatura, String colorPredominante, Boolean genero, Integer numeroPatas, String dieta, String clasificacion) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.peso = peso;
+        this.estatura = estatura;
+        this.colorPredominante = colorPredominante;
+        this.genero = genero;
+        this.numeroPatas = numeroPatas;
+        this.dieta = dieta;
+        this.clasificacion = clasificacion;
+    }
+
+    //METODOS ORDINARIOS
+
+}
